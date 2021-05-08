88a55e7e84431d602316deec3bcaa4042e8b5574
Tunahan <tunahan.aydinoglu@yemeksepeti.com>
2021-05-08 15:24:53 +0300

    image update

--- routes/words.js
+++ routes/words.js
@@ -1,24 +1,24 @@
 const express = require("express");
 
 const {
-    checkUserWordsExist,
+  checkUserWordsExist,
 } = require("../middlewares/database/databaseErrorHelpers");
 
 const {
-    getAllUserWords,
-    getAllUserLastWords,
-    addNewUserWords
+  getAllUserWords,
+  getAllUserLastWords,
+  addNewUserWords
 } = require("../controllers/userWords");
 
 const {
   getAccessToRoute,
 } = require("../middlewares/authorization/auth");
 
-const router = express.Router({mergeParams:true});
+const router = express.Router({ mergeParams: true });
 
-router.get("/",getAccessToRoute,getAllUserWords);
+router.get("/", getAccessToRoute, getAllUserWords);
 
-router.get("/last",getAccessToRoute,getAllUserLastWords);
+router.get("/last", getAccessToRoute, getAllUserLastWords);
 
 // router.get("/quiz",getAllUserLastWords);
 
