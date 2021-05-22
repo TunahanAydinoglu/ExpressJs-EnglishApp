7517a485568266e86793cf54dfbb4365a59aa8a6
Tunahan <tunahan.aydinoglu@yemeksepeti.com>
2021-05-22 18:27:57 +0300

    add token need

--- routes/translateVocabulary.js
+++ routes/translateVocabulary.js
@@ -2,9 +2,12 @@ const express = require("express");
 
 
 const { postTranslateVocabulary } = require("../controllers/translateVocabulary");
+const {
+    getAccessToRoute,
+  } = require("../middlewares/authorization/auth");
 
 const router = express.Router({ mergeParams: true });
 
-router.post("/", postTranslateVocabulary);
+router.post("/",getAccessToRoute, postTranslateVocabulary);
 
 module.exports = router;
